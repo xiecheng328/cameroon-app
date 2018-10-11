db1ab9ef693cecfd890a1a72d7c8333aba83eae3
Cyberhan123 <255542417@qq.COM>
2018-10-11 22:29:20 +0800

    完整引入muse-ui

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/blog">Blog</router-link>|
-      <router-link to="/message">Message</router-link> |
-      <router-link to="/shop">Shop</router-link>|
-      <router-link to="/profile">Profile</router-link>
+    <div id="app">
+        <mu-container>
+        <router-view/>
+        <div id="nav">
+            <router-link to="/blog">Blog</router-link>
+            |
+            <router-link to="/message">Message</router-link>
+            |
+            <router-link to="/shop">Shop</router-link>
+            |
+            <router-link to="/profile">Profile</router-link>
+        </div>
+        </mu-container>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    #nav {
+        padding: 30px;
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+
+Vue.use(MuseUI);
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
