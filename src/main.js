b793acdf5ddfa431dc3c1616d6b39349d7ceb51d
Cyberhan123 <255542417@qq.COM>
2018-10-13 20:23:26 +0800

    移除添加mockAll的注入

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import MuseUI from 'muse-ui';
 import 'muse-ui/dist/muse-ui.css';
 //将mock文件下所有文件引入
 import 'mockjs';
-import './mockAll';
+//import './mockAll';
 Vue.config.productionTip = false;
 
 Vue.use(MuseUI);
