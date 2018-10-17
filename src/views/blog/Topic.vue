137cc13bfb58c438268b5ee0514fca849a08583b
Cyberhan123 <255542417@qq.COM>
2018-10-17 14:31:35 +0800

    调整顶部栏

--- src/views/blog/Topic.vue
+++ src/views/blog/Topic.vue
@@ -1,12 +1,12 @@
 <template>
     <!--韩瀚-->
-    <div id="cameroon-app">
-        <mu-appbar style="width: 100%;" title="">
+    <div id="cameroon-topic">
+        <mu-appbar style="width: 100%;height: 30px;" title="">
             <mu-container class="button-wrapper">
                 <mu-button flat to="/blog/topic/fellow">关注</mu-button>
                 <mu-button flat to="/blog/topic/recommend">推荐</mu-button>
                 <mu-button flat to="/blog/topic/hotlist">热榜</mu-button>
-                <mu-button flat to="/blog/topic/alists">回答</mu-button>
+                <!--<mu-button flat to="/blog/topic/alists">回答</mu-button>-->
             </mu-container>
         </mu-appbar>
         <router-view></router-view>
@@ -20,14 +20,24 @@
 </script>
 
 <style scoped lang="scss">
+    .mu-elevation-4{
+        -webkit-box-shadow:none;
+        box-shadow:none;
+    }
+    .mu-appbar{
+        background-color: #ffffff;
+    }
+    #cameroon-topic{
+    background: #eeeeee;
+    }
     .button-wrapper {
         text-align: center;
         .mu-button{
             display: block;
             float: left;
             /*margin: 20px;*/
-            width: 75px;
-            height: 56px;
+            width: 103px;
+            height: 30px;
         }
         .router-link-active{
             border-bottom: solid 2px black;
