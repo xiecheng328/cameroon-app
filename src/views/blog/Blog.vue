101b224a367cbeaf0d2149129286951647478e52
Cyberhan123 <255542417@qq.COM>
2018-10-17 14:01:42 +0800

    使底栏完全贴靠底部

--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -3,7 +3,7 @@
         <!--<h1>This is a message page</h1>-->
         <router-link to="/blog/details">全部关注</router-link>
         |
-        <router-link to="/blog/topic">话题</router-link>
+        <router-link to="/blog/topic/recommend">话题</router-link>
         <router-view></router-view>
     </div>
 </template>
