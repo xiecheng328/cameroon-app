32d027408499fa90711f67dc361961d5bc4c9cc5
tanjiuyusila <521196748@qq,com>
2018-10-18 16:28:38 +0800

    search页面跳转修改

--- src/components/blog/CommonHeader.vue
+++ src/components/blog/CommonHeader.vue
@@ -2,7 +2,8 @@
     <mu-appbar style="width: 100%; height: 50px;">
         <mu-button flat to="/blog/details" slot="left">全部关注</mu-button>
         <mu-button flat to="/blog/topic/recommend"  slot="left">话题</mu-button>
-        <mu-auto-complete></mu-auto-complete>
+        <!--search路由跳转--修改-->
+        <mu-auto-complete @click="jumptoSearch"></mu-auto-complete>
         <mu-button icon slot="right">
             <mu-icon value="search"></mu-icon>
         </mu-button>
@@ -11,7 +12,14 @@
 
 <script>
     export default {
-        name: "CommonHeader"
+        name: "CommonHeader",
+        methods:{
+            //search跳转页面路由
+            jumptoSearch(){
+                this.$router.push('/blog/search')
+            }
+            //search跳转页面路由
+        }
     }
 </script>
 
