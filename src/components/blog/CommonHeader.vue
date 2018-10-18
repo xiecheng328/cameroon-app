9b266ebe8cc7d4c1e032f46eafe3c20d775b8342
LG-17864078090 <17864078090@163.COM>
2018-10-18 19:38:21 +0800

    2018.10.15

--- src/components/blog/CommonHeader.vue
+++ src/components/blog/CommonHeader.vue
@@ -1,12 +1,9 @@
 <template>
-    <mu-appbar style="width: 100%; height: 50px;">
-        <mu-button flat to="/blog/details" slot="left">全部关注</mu-button>
-        <mu-button flat to="/blog/topic/recommend"  slot="left">话题</mu-button>
-        <!--search路由跳转--修改-->
-        <mu-auto-complete @click="jumptoSearch"></mu-auto-complete>
-        <mu-button icon slot="right">
-            <mu-icon value="search"></mu-icon>
-        </mu-button>
+    <mu-appbar class="head">
+        <mu-button class="camera" flat to="">111</mu-button>
+        <mu-button class="allAttention" flat to="/blog/details">全部关注</mu-button>
+        <mu-button class="topic" flat to="/blog/topic/recommend">话题</mu-button>
+        <mu-button class="add" flat to="">222</mu-button>
     </mu-appbar>
 </template>
 
@@ -14,15 +11,25 @@
     export default {
         name: "CommonHeader",
         methods:{
-            //search跳转页面路由
-            jumptoSearch(){
-                this.$router.push('/blog/search')
-            }
-            //search跳转页面路由
+
         }
     }
 </script>
 
 <style scoped>
+
+    .head .camera{
+        width: 15%;
+    }
+    .head .allAttention{
+        width: 35%;
+    }
+    .head .topic{
+        width: 35%;
+    }
+    .head .add{
+        width: 15%;
+    }
+
 
 </style>
