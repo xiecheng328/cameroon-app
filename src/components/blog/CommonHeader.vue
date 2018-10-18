5fdecb8727dae9808bfa749007f361d15be41cdf
tanjiuyusila <521196748@qq,com>
2018-10-18 21:05:12 +0800

    Merge branch 'master' of D:\学习资料\前端\yzl\cameroon-app with conflicts.

--- src/components/blog/CommonHeader.vue
+++ src/components/blog/CommonHeader.vue
@@ -1,13 +1,15 @@
 <template>
     <mu-appbar class="head">
-        <mu-button class="camera" flat to="">
-            <img src="../../assets/img/相机.png" alt="">
-        </mu-button>
+        <mu-button class="camera" flat to="">111</mu-button>
         <mu-button class="allAttention" flat to="/blog/details">全部关注</mu-button>
         <mu-button class="topic" flat to="/blog/topic/recommend">话题</mu-button>
-        <mu-button class="add" flat to="">
-            <img src="../../assets/img/加号.png" alt="">
+        <mu-button class="add" flat to="">222</mu-button>
+        <!--search路由跳转--修改-->
+        <mu-auto-complete @click="jumptoSearch"></mu-auto-complete>
+        <mu-button icon slot="right">
+            <mu-icon value="search"></mu-icon>
         </mu-button>
+        <!---->
     </mu-appbar>
 </template>
 
@@ -15,6 +17,11 @@
     export default {
         name: "CommonHeader",
         methods:{
+            //search跳转页面路由
+            jumptoSearch(){
+                this.$router.push('/blog/search')
+            }
+            //search跳转页面路由
         }
     }
 </script>
@@ -25,15 +32,14 @@
         width: 15%;
     }
     .head .allAttention{
-        font-size: 16px;
         width: 35%;
     }
     .head .topic{
-        font-size: 16px;
         width: 35%;
     }
     .head .add{
         width: 15%;
     }
+
 
 </style>
