31e3bfeb01522016f2fbfd3b139420107355d4cb
xiejunjie629 <972118604@qq.com>
2018-10-18 20:07:11 +0800

    XJJ--Profile/Myfans

--- src/views/profile/childrenpage/Myfans/Myfans.vue
+++ src/views/profile/childrenpage/Myfans/Myfans.vue
@@ -1,6 +1,40 @@
 <template>
-  <div class="message">
-    <h1>This is a Myfans page</h1>
-  </div>
+    <div>
+        <mu-container>
+        <mu-tabs :value.sync="active1" inverse color="secondary" text-color="rgba(0, 0, 0, .54)"  center>
+            <mu-tab >粉丝年龄集中</mu-tab>
+            <mu-tab >粉丝性别分布</mu-tab>
+            <mu-tab >粉丝数排名</mu-tab>
+        </mu-tabs>
+        <div class="demo-text" v-if="active1 === 0">
+            <p>“粉丝年龄集中”</p>
+        </div>
+        <div class="demo-text" v-if="active1 === 1">
+            <p>“粉丝性别分布”</p>
+        </div>
+        <div class="demo-text" v-if="active1 === 2">
+            <p>“粉丝数排名”</p>
+        </div>
+        </mu-container>
+    </div>
 </template>
 
+<script>
+export default {
+    name: "profile",
+  data () {
+    return {
+      active1: 0
+    };
+  }
+}
+</script>
+<style lang="scss">
+.demo-text {
+  padding: 16px;
+  background: #fff;
+  p {
+    margin: 8px 0;
+  }
+}
+</style>
