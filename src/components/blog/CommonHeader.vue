82c1cf326f1560cdfbabc6ffd2696637f820677f
LG-17864078090 <17864078090@163.COM>
2018-10-18 19:57:21 +0800

    2018.10.18

--- src/components/blog/CommonHeader.vue
+++ src/components/blog/CommonHeader.vue
@@ -1,9 +1,13 @@
 <template>
     <mu-appbar class="head">
-        <mu-button class="camera" flat to="">111</mu-button>
+        <mu-button class="camera" flat to="">
+            <img src="../../assets/img/相机.png" alt="">
+        </mu-button>
         <mu-button class="allAttention" flat to="/blog/details">全部关注</mu-button>
         <mu-button class="topic" flat to="/blog/topic/recommend">话题</mu-button>
-        <mu-button class="add" flat to="">222</mu-button>
+        <mu-button class="add" flat to="">
+            <img src="../../assets/img/加号.png" alt="">
+        </mu-button>
     </mu-appbar>
 </template>
 
@@ -19,17 +23,22 @@
 <style scoped>
 
     .head .camera{
+
         width: 15%;
     }
     .head .allAttention{
+        font-size: 16px;
+        
         width: 35%;
     }
     .head .topic{
+
+        font-size: 16px;
+
         width: 35%;
     }
     .head .add{
         width: 15%;
     }
-
 
 </style>
