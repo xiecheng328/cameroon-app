d80e027df754e7b6e8b4b67ce17d6a87ae5a55e1
yujingxiacai <31340485+yujingxiacai@users.noreply.github.com>
2018-10-18 19:40:17 +0800

    profile修改了路由(syk)

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="profile">
+    <router-view></router-view>
+    <div v-show="isClick">
+
     <h3 class="profile-header">我的</h3>
     <div class="profile-user">
       <div class="profile-user-slideimg"><img src="@/assets/img/user.png" alt="" class="profile-user-slideimg-uersimg"></div>
@@ -7,10 +10,9 @@
     </div>
     <div class="menuprofile">
       <div class="menuprofile-first">
-        <li v-for="(item,index) in profileMenu" :key="index" @click="routerGo"><img :src="item.iconimg" alt="" ><span>{{item.text}}</span></li>
+        <li v-for=" item in profileMenu" :key="item.id" @click="routerGo(item)"><img :src="item.iconimg" alt="" ><span>{{item.text}}</span></li>
       </div>
-      <div class="menuprofile-second"></div>
-      <div class="menuprofile-third"></div>
+    </div>
     </div>
   </div>
 </template>
@@ -24,38 +26,56 @@ import 'muse-ui/dist/muse-ui.css';
 export default {
    data(){
      return{
+       isClick:true,
        profileMenu: [{
          iconimg:"/img/profileicon1.png",
-         text:"我的关注"
+         text:"我的关注",
+         src:"Myinterst",
+         id:1,
+
        },{
         iconimg:"/img/profileicon1.png",
-         text:"我的粉丝"
+         text:"我的粉丝",
+         src:"Myfans",
+         id:2
        },{
         iconimg:"/img/profileicon1.png",
-         text:"我的购买记录"
+         text:"我的购买记录",
+         src:"Myshopping",
+         id:3
        },{
          iconimg:"/img/profileicon1.png",
-         text:"认证通道"
+         text:"认证通道",
+         src:"Identification",
+         id:4
        },{
         iconimg:"/img/profileicon1.png",
-         text:"我的下载"
+         text:"我的下载",
+         src:"Mydownload",
+         id:5
        },{
         iconimg:"/img/profileicon1.png",
-         text:"设置"
+         text:"设置",
+         src:"Myset",
+         id:6
        },{
          iconimg:"/img/profileicon1.png",
-         text:"帮助与反馈"
+         text:"帮助与反馈",
+         src:"HelpAndfeedback",
+         id:7
        }],
      }
    },
    created() {
-
    },
   
 
    
    methods:{
-    routerGo(){
+    routerGo(a){
+      this.isClick=false;
+      this.$router.push({ name:`${a.src}`, params: { userId: 123 }})
+      console.log(a);
       
     },
    }
@@ -127,8 +147,6 @@ export default {
       padding-bottom: 14px;
     }
   }
-  &-first{
- 
-  }
+
 }
 </style>
