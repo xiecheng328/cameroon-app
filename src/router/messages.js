aef96f2eb588e235b62182e88cee235b0a9a166b
Lsunshineandrainbow <3480782290@qq.com>
2018-10-19 14:38:37 +0800

    Merge branch 'master' of D:\cameroon-app with conflicts.

--- src/router/messages.js
+++ src/router/messages.js
@@ -2,6 +2,7 @@
  * Created by apple on 18/10/11.
  * message(消息)模块子路由
  */
+import Withme from '@/views/message/Withme(李晶).vue';
 export default [
-
+    {path:'withme',component:Withme}
 ]
--- src/router/profiles.js
+++ src/router/profiles.js
@@ -2,52 +2,7 @@
  * Created by apple on 18/10/11.
  * profile(我的)模块子路由
  */
-// import Vue from 'vue';
-// import Router from './vue-router';
-// import CommonFooter from '@/components/common/CommonFooter.vue'
-import HelpAndfeedback from '@/views/profile/childrenpage/HelpAndfeedback/HelpAndfeedback.vue';
-import Identification from '@/views/profile/childrenpage/Identifiction/Identifict.vue';
-import Mydownload from '@/views/profile/childrenpage/Mydownload/Mydownload.vue';
-import Myfans from '@/views/profile/childrenpage/Myfans/Myfans.vue';
-import Myinterst from '@/views/profile/childrenpage/Myinterst/Myinterst.vue';
-import Myset from '@/views/profile/childrenpage/MySet/Myset.vue';
-import Myshopping from '@/views/profile/childrenpage/Myshopping/Myshopping.vue';
-
-// Vue.use(Router);
+import Withme from '@/views/message/Withme(李晶).vue';
 export default [
-    // mode: 'history',
-    // base: process.env.BASE_URL,
-   
-       {
-        path: '/myinterst',
-        name: 'Myinterst',
-        component: Myinterst,
-      },
-      {
-        path: '/myfans',
-        name: 'Myfans',
-        component:Myfans,
-      }, {
-        path: '/myshopping',
-        name: 'Myshopping',
-        component: Myshopping,
-      }, {
-        path: '/identification',
-        name: 'Identification',
-        component: Identification,
-      }, {
-        path: '/mydownload',
-        name: 'Mydownload',
-        component: Mydownload,
-      }, {
-        path: '/myset',
-        name: 'Myset',
-        component: Myset,
-      },
-      {
-        path: '/helpAndfeedback',
-        name: 'HelpAndfeedback',
-        component: HelpAndfeedback,
-      },
-    
-];
+
+]
