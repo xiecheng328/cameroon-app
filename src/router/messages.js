620c2fad8d4d6bf227443ae000c573262b14a6c2
Lsunshineandrainbow <3480782290@qq.com>
2018-10-19 16:45:20 +0800

    @与我相关

--- src/router/messages.js
+++ src/router/messages.js
@@ -6,10 +6,11 @@ import OpenSubScription_content from '../views/message/ChildrenPages/subScriptio
 import OpenSubScription_message from '../views/message/ChildrenPages/subScription/openSubScription_message';
 import SubScription_content from '../views/message/ChildrenPages/subScription/subScription_content';
 import SubScription_message from '../views/message/ChildrenPages/subScription/subScription_message';
-
+import Withme from '@/views/message/Withme.vue';
 export default [
     {path:'openSubScription_content',component:OpenSubScription_content},
     {path:'openSubScription_message',component:OpenSubScription_message},
     {path:'subScription_content',component:SubScription_content},
-    {path:'subScription_message',component:SubScription_message}
+    {path:'subScription_message',component:SubScription_message},
+    {path:'withme',component:Withme}
 ]
