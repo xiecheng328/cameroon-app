7c9cfe11a343f30512617e9fc8c72c99645dcca5
Cyberhan123 <255542417@qq.COM>
2018-10-19 15:37:17 +0800

    init dev

--- src/components/topic/TopicHeader.vue
+++ src/components/topic/TopicHeader.vue
@@ -3,6 +3,7 @@
     <mu-appbar style="width: 100%; height: 50px;">
         <mu-button flat to="/blog/details" slot="left">全部关注</mu-button>
         <mu-button flat to="/blog/topic/recommend"  slot="left">话题</mu-button>
+        <mu-button flat to="/blog/topic/putquestion" slot="left">提问</mu-button>
         <!--search路由跳转--修改-->
         <mu-button icon slot="right">
             <mu-icon value="search" @click="jumptoSearch"></mu-icon>
