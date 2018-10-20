d442a171538ca637ee33ab8dfe10f59bc4e8c8a4
wangyutingting518 <971195060@qq.com>
2018-10-20 20:23:24 +0800

    passage

--- src/router/blogs.js
+++ src/router/blogs.js
@@ -15,6 +15,7 @@ import RepostWb from '@/views/blog/view/RepostWb.vue';
 import Talk from '@/views/blog/view/Talk.vue';
 import Forwaid from '@/views/blog/view/Forwaid.vue';
 import report from '@/views/blog/view/report.vue'
+import Forword from '@/views/blog/view/Forword.vue'
 export default [
     {path: '/blog', redirect: "/blog/details"},
     {path: 'sendweibo', component: SendWeibo},
@@ -22,6 +23,7 @@ export default [
     {path: '/likeweibo', component: LikeWeibo,name: 'LikeWeibo'},
     {path: '/commentwb', component: CommentWb,name: 'CommentWb'},
     {path: '/repostwb', component: CommentWb,name: 'RepostWb'},
+    {path: '/forword', component: Forword,name: 'Forword'},
     {path: 'passage',redirect:"passage/comment"},
     {path: 'passage', component: Passage,children:[
             {path: 'comment', component: report},
---
+++ src/views/blog/view/Forword.vue
@@ -0,0 +1,189 @@
+<template>
+    <div class="apps">
+        <!--<div  class="app2">-->
+        <button class="app7"><router-link to="/blog/details" class="ys">取消</router-link></button>
+        <div class="zhuanfa">转发圈子</div>
+        <button class="fasong">发送</button>
+        <span class="xian"></span>
+        <!--</div>-->
+        <input class="pinglun"  type="text"value="分享心得...">
+        <input class="kuang" type="checkbox" />
+        <ul class="ziti">
+            <span>同时转发</span></ul>
+        <div class="color">
+            <div>
+                <li class="tupian"><img src="../../../assets/img/图层 2.png" height="180" width="180"/></li>
+            </div>
+            <div class="word">
+                <h3 class="topic">分享标题</h3>
+                <span class="content">分享内容</span>
+            </div>
+
+        </div>
+        <div class="app-bottom">
+            <div class="kongbai">
+                <span></span>
+                <button  class="zhaopian" ><img src="../../../assets/img/图层 4.png" /></button>
+                <button  class="biaoqing" ><img src="../../../assets/img/图层 5.png" /></button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+
+        name:"Forwaid"
+
+    }
+</script>
+<style scoped lang="scss">
+    .apps{
+        background: #ffffff;
+    }
+    .app1 {
+        width: 6.4rem;
+        height: 40px;
+        background-color: #ffffff;
+        text-align:center;
+    }
+    .app7 {
+        float: left;
+        margin: 0;
+        padding: 0;
+        border: 1px solid transparent;
+        background:none;
+        position: absolute;
+        left: 10px;
+        top: 32px;
+        color: #333333;
+    }
+    .zhuanfa {
+        height: 29px;
+        font-size: 20px;
+        float:left;
+        position: absolute;
+        left: 50%;
+        top: 30px;
+        margin-left: -10%;
+        line-height: 26px;
+        color: #333333;
+    }
+    .fasong{
+        margin: 0;
+        padding: 0;
+        background:none;
+        color: #999999;
+        float: right;
+        position: absolute;
+        right: 3%;
+        top: 30px;
+        width: 13%;
+        border-radius: 5px;
+        border: solid 1px #999999;
+    }
+    .pinglun{
+        border:none;
+        position: absolute;
+        top: 80px;
+        left: 10px;
+        background:none;
+    }
+    .app-bottom{
+        position: fixed;
+        bottom: 0;
+        left:0;
+    }
+    .kongbai{
+        width: 6.4rem;
+        height: 50px;
+        background:none;
+        border-top: 1px solid #cccccc;
+
+    }
+    .xian {
+        width: 6.4rem;
+        height: 1px;
+        background-color: #dddddd;
+        position: absolute;
+        top: 70px;
+        content: '';
+        border-bottom: 1px solid #cccccc;
+        transform-origin: 0 100%;
+        transform: scaleY(.5);
+    }
+    .zhaopian {
+        background: white;
+        position: absolute;
+        left: 10%;
+        top: 15px;
+        margin: 0;
+        padding: 0;
+        border: 1px solid transparent;
+        float: left;
+        height: 25px;
+        width: 25px;
+    }
+    .biaoqing{
+        margin: 0;
+        padding: 0;
+        background: white;
+        border: 1px solid transparent;
+        float: left;
+        position: relative;
+        left: 30%;
+        top: 15px;
+        height: 25px;
+        width: 25px;
+    }
+    .kuang{
+        position: absolute;
+        left: 2%;
+        bottom: 55px;
+    }
+    .ziti{
+        font-size: 15px;
+        letter-spacing: 0px;
+        color: #999999;
+        position: absolute;
+        left: 7%;
+        bottom: 48px;
+        margin: 0;
+        padding: 0;
+    }
+    .ys{
+        color: #333333;
+    }
+    .color {
+        background: #EEEEEE;
+        width: 100%;
+
+        margin-top:75%;
+        display:flex;
+        margin-left: 8px;
+        margin-right: 8px;
+
+    }
+    .color img{
+        float:left;
+        margin:10px;
+        flex:5%;
+        width:65%;
+        text-align: center;
+
+    }
+    .color .word{
+        flex:95%;
+        left: 1%;
+    }
+    .color .topic{
+        font-size: 18px;
+        color: #333333;
+    }
+    .color .content{
+        font-size: 16px;
+        color:#cccc;
+    }
+</style>
+<style scoped lang="scss">
+
+</style>
