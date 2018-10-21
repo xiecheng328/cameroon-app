a6090188e31671e7cc96922b9c32b4ec0cf71269
liuyue182 <473404893@qq.com>
2018-10-21 12:32:45 +0800

    message-chatroom

--- src/views/message/ChildrenPages/subScription/subScription_message.vue
+++ src/views/message/ChildrenPages/subScription/subScription_message.vue
@@ -1,10 +1,10 @@
 <template>
     <li>
-        <div class="left">
-            <img src="../../../../assets/img/message_30a07a3203b47979cf5519981729f3d8.jpg" alt="">
-        </div>
-        <div class="right">
-            <span class="name">{{list.name}}</span>
+        <img src="../../../../assets/img/message_30a07a3203b47979cf5519981729f3d8.jpg" alt="">
+        <div @click="subNext">
+            <span class="user_name">
+                {{list.name}}
+            </span>
             <p class="content">{{list.content}}</p>
         </div>
     </li>
@@ -13,36 +13,37 @@
 <script>
     export default {
         name: "subScription_message",
-        props:["list"]
+        props:["list"],
+        methods:{
+            subNext(){
+                this.$router.push({
+                    path:'/openSubScription_content'
+                })
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
     li{
         display: flex;
-        height: 60px;
+        height: 1.3rem;
         background-color:#ead4d4;
         margin-top: 5px;
-        & .left{
-            flex:0.6;
-            text-align: center;
-            margin-top: 10px;
-        }
-        & .right{
-            flex: 3;
-        }
-        & img{
-            height: 40px;
-            width:40px;
-            border-radius: 50%;
-        }
-        & .scrip{
-            font-size: 17px;
-        }
-        & .content{
-            font-size: 12px;
-            color: #605b5b;
-            margin: 0;
-        }
     }
+    img{
+        height: 1.3rem;
+        width: 1.3rem;
+    }
+    .user_name{
+        font-size: 16px;
+        padding-left: 0.2rem;
+    }
+    .content{
+        overflow: hidden;
+        width: 100%;
+        padding-left: 0.2rem;
+        padding-top: 0.1rem;
+    }
+
 </style>
