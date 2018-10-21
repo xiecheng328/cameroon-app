69b8970e335cd0ab2d470dfadb1d7330280627ad
zhangshuang-h <781792980@qq.com>
2018-10-21 21:53:35 +0800

    message

--- src/router/messages.js
+++ src/router/messages.js
@@ -6,6 +6,7 @@ import OpenSubScription_content from '../views/message/ChildrenPages/subScriptio
 import OpenSubScription_message from '../views/message/ChildrenPages/subScription/openSubScription_message';
 import SubScription_content from '../views/message/ChildrenPages/subScription/subScription_content';
 import SubScription_message from '../views/message/ChildrenPages/subScription/subScription_message';
+import Unattended_news from '../views/message/ChildrenPages/UnAttention/Unattended-news';
 import Report from '../views/message/report';
 import CopyList from '../views/message/copyList';
 import Newdetail from '../views/message/newdetail';
@@ -17,6 +18,7 @@ export default [
     {path:'/openSubScription_message',component:OpenSubScription_message},
     {path:'/subScription_content',component:SubScription_content},
     {path:'/subScription_message',component:SubScription_message},
+    { path:"/Unattended_news" ,component:Unattended_news},
     {
         path:'/newdetail',component:Newdetail
     },
