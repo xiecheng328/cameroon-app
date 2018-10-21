ff67e6a015f1d9f0636173b3e788adff96df4a12
zhangshuang-h <781792980@qq.com>
2018-10-21 22:11:26 +0800

    message

--- src/router/messages.js
+++ src/router/messages.js
@@ -9,6 +9,9 @@ import SubScription_message from '../views/message/ChildrenPages/subScription/su
 import Unattended_news from '../views/message/ChildrenPages/UnAttention/Unattended-news';
 import Report from '../views/message/report';
 import CopyList from '../views/message/copyList';
+import Newdetail from "../views/message/newdetail"
+import Withme from "../views/message/Withme"
+import  Chatroom from "../views/message/ChildrenPages/UnAttention/Chatroom"
 
 export default [
     {path:'/openSubScription_content',component:OpenSubScription_content},
