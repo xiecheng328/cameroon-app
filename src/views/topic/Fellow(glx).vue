fa59fc1df48f681a3a6c3ab1ef1e6d2e20600de0
glx <1480059245@qq.com>
2018-10-22 14:30:14 +0800

    0

--- src/views/topic/Fellow(glx).vue
+++ src/views/topic/Fellow(glx).vue
@@ -40,7 +40,81 @@
                     {
                         focusPeople:"ritle",
                         time:"四分钟前",
-                        title:"大学生如何在堕落寝室",
+                        title:"如何加强农业发展",
+                        videoSrc:"",
+                        author:"rose",
+                        article:"现代农业发展的一个重要指标就是农业机械化的发展。农业机械化发展是发展现代农业、" +
+                            "推进农业产业化、集约化，推进农村经济发展的重要条件。",
+                        agreeNum:7878,
+                        commentNum:96696,
+                        imgSrc:"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2856070669,1185745263&fm=27&gp=0.jpg",
+                        isShowVideo:false,
+                        isShowImg:true
+
+
+
+                    },
+                    {
+                        focusPeople:"json",
+                        time:"八分钟前",
+                        title:"推广机制落后",
+                        videoSrc:"",
+                        author:"Micheal",
+                        article:"在市场经济体系不断完善的基础上，农机市场得以全面开放，" +
+                            "这种开放式的农机市场使原有的农机推广方式发生了一定的变革，" +
+                            "农民不再是农业机械技术推广的被动接受者，而是变被动为主动，" ,
+                        agreeNum:366,
+                        commentNum:12,
+                        imgSrc:"",
+                        isShowVideo:false,
+                        isShowImg:false
+
+
+                    }, {
+                        focusPeople:"json",
+                        time:"七分钟前",
+                        title:"推广信息内容落后",
+                        videoSrc:"./../../test.mp4",
+                        author:"steven",
+                        article:"部分乡镇存在忽视农业经济发展的现象，由于对农业发展缺乏重视，在进行农业生产时还采用原有的技术，没有根据农业技术的发展进行技术更新，致使农业发展长时间停留在一个阶段。",
+                        agreeNum:2155,
+                        commentNum:7588,
+                        imgSrc:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3375143504,581016811&fm=27&gp=0.jpg",
+                        isShowVideo:true,
+                        isShowImg:false
+
+                    },
+                    {
+                        focusPeople:"ritle",
+                        time:"四分钟前",
+                        title:"乡镇农业机械技术推广主体配备差",
+                        videoSrc:"",
+                        author:"rose",
+                        article:"虽然，近些年我国已经认识到农业经济发展的重要性，" +
+                            "针对农业机械技术的推广工作给予了更多的支持，" ,
+                        agreeNum:7878,
+                        commentNum:96696,
+                        imgSrc:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=684603144,15627662&fm=200&gp=0.jpg",
+                        isShowVideo:false,
+                        isShowImg:true
+                    },{
+                        focusPeople:"json",
+                        time:"七分钟前",
+                        title:"今天开始收割",
+                        videoSrc:"./../../test.mp4",
+                        author:"steven",
+                        article:"机器不错，天气也很好，稻子很干",
+                        agreeNum:2155,
+                        commentNum:7588,
+                        imgSrc:"",
+                        isShowVideo:true,
+                        isShowImg:false
+
+                    },
+                    {
+                        focusPeople:"ritle",
+                        time:"四分钟前",
+                        title:"如何加强农业发展",
                         videoSrc:"",
                         author:"rose",
                         article:"现代农业发展的一个重要指标就是农业机械化的发展。农业机械化发展是发展现代农业、" +
@@ -188,81 +262,7 @@
                     {
                         focusPeople:"ritle",
                         time:"四分钟前",
-                        title:"大学生如何在堕落寝室",
-                        videoSrc:"",
-                        author:"rose",
-                        article:"现代农业发展的一个重要指标就是农业机械化的发展。农业机械化发展是发展现代农业、" +
-                            "推进农业产业化、集约化，推进农村经济发展的重要条件。",
-                        agreeNum:7878,
-                        commentNum:96696,
-                        imgSrc:"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2856070669,1185745263&fm=27&gp=0.jpg",
-                        isShowVideo:false,
-                        isShowImg:true
-
-
-
-                    },
-                    {
-                        focusPeople:"json",
-                        time:"八分钟前",
-                        title:"推广机制落后",
-                        videoSrc:"",
-                        author:"Micheal",
-                        article:"在市场经济体系不断完善的基础上，农机市场得以全面开放，" +
-                            "这种开放式的农机市场使原有的农机推广方式发生了一定的变革，" +
-                            "农民不再是农业机械技术推广的被动接受者，而是变被动为主动，" ,
-                        agreeNum:366,
-                        commentNum:12,
-                        imgSrc:"",
-                        isShowVideo:false,
-                        isShowImg:false
-
-
-                    }, {
-                        focusPeople:"json",
-                        time:"七分钟前",
-                        title:"推广信息内容落后",
-                        videoSrc:"./../../test.mp4",
-                        author:"steven",
-                        article:"部分乡镇存在忽视农业经济发展的现象，由于对农业发展缺乏重视，在进行农业生产时还采用原有的技术，没有根据农业技术的发展进行技术更新，致使农业发展长时间停留在一个阶段。",
-                        agreeNum:2155,
-                        commentNum:7588,
-                        imgSrc:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3375143504,581016811&fm=27&gp=0.jpg",
-                        isShowVideo:true,
-                        isShowImg:false
-
-                    },
-                    {
-                        focusPeople:"ritle",
-                        time:"四分钟前",
-                        title:"乡镇农业机械技术推广主体配备差",
-                        videoSrc:"",
-                        author:"rose",
-                        article:"虽然，近些年我国已经认识到农业经济发展的重要性，" +
-                            "针对农业机械技术的推广工作给予了更多的支持，" ,
-                        agreeNum:7878,
-                        commentNum:96696,
-                        imgSrc:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=684603144,15627662&fm=200&gp=0.jpg",
-                        isShowVideo:false,
-                        isShowImg:true
-                    },{
-                        focusPeople:"json",
-                        time:"七分钟前",
-                        title:"今天开始收割",
-                        videoSrc:"./../../test.mp4",
-                        author:"steven",
-                        article:"机器不错，天气也很好，稻子很干",
-                        agreeNum:2155,
-                        commentNum:7588,
-                        imgSrc:"",
-                        isShowVideo:true,
-                        isShowImg:false
-
-                    },
-                    {
-                        focusPeople:"ritle",
-                        time:"四分钟前",
-                        title:"大学生如何在堕落寝室",
+                        title:"如何加强农业发展",
                         videoSrc:"",
                         author:"rose",
                         article:"现代农业发展的一个重要指标就是农业机械化的发展。农业机械化发展是发展现代农业、" +
