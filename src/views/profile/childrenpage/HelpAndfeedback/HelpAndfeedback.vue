91ddc34e0e1f6803e94abffb6d124443d566411d
TIANMAO - FIRST <40513247+MarcusYZ@users.noreply.github.com>
2018-10-24 00:40:15 +0800

    Update HelpAndfeedback.vue

--- src/views/profile/childrenpage/HelpAndfeedback/HelpAndfeedback.vue
+++ src/views/profile/childrenpage/HelpAndfeedback/HelpAndfeedback.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="HelpAndfeedback">
+  <div class="Commonquestion">
     
     <router-view v-show="!isClick" v-on:resetFatherp="resetF" ></router-view>
       <div v-show="isClick">
-    <h3 class="HelpAndfeedback-header">帮助与反馈</h3>
-   
-    <div class="menuHelpAndfeedback">
-      <div class="menuHelpAndfeedback-first">
-        <li v-for=" item in HelpAndfeedback" :key="item.id" @click="routerGo(item)"><img :src="item.iconimg" alt="" ><span>{{item.text}}</span></li>
-      </div>
-    </div>
+      <div class="Commonquestion-title">
+         <mu-button class="download-title-back" flat @click="changeFatherB">{{pageMessage.return}}</mu-button>
+                  <h3 class="Commonquestion-title-text">{{pageMessage.commonquestion}}</h3>
+                 
+              </div>
+               <h4>{{pageMessage.commonquestionFirst}}</h4>
+     <h6>{{pageMessage.commonanswerFirst}}</h6>
+     <h4>{{pageMessage. commonquestionSecond}}</h4>
+    <h6>{{pageMessage.commonanswerSecond}}</h6>
+     <h4>{{pageMessage.commonquestionThird}}</h4>
+      <h6>{{pageMessage.commonanswerThird}}</h6>
+     <h4>{{pageMessage.commonquestionForth}}</h4>
+      <h6>{{pageMessage.commonanswerForth}}</h6>
+    
+      <h4>{{pageMessage.commonquestionFifth}}</h4>
+      <h6>{{pageMessage.commonanswerFifth}}</h6>
     </div>
  </div>
 </template>
@@ -24,23 +33,26 @@ export default {
    data(){
      return{
        isClick:true,
-       HelpAndfeedback: [{
-         iconimg:"/img/profileicon1.png",
-         text:"用户常见问题",
-         src:"Commonquestion",
-         id:1,
+        pageMessage:{
+        return:this.$t('profile.return'),
+        commonquestion:this.$t('profile.commonquestion'),
+       commonquestionFirst:this.$t('profile.commonquestionFirst'),
+       commonanswerFirst:this.$t('profile.commonanswerFirst'),
+       commonquestionSecond:this.$t('profile.commonquestionSecond'),
+       commonanswerSecond:this.$t('profile.commonanswerSecond'),
+       commonquestionThird:this.$t('profile.commonquestionThird'),
+       commonanswerThird:this.$t('profile.commonanswerThird'),
+       commonquestionForth:this.$t('profile.commonquestionForth'),
+      commonanswerForth:this.$t('profile.commonanswerForth'),
+      commonquestionFifth:this.$t('profile.commonquestionFifth'),
+       commonanswerFifth:this.$t('profile.commonanswerFifth'),
+       
 
-       },{
-        iconimg:"/img/profileicon1.png",
-         text:"用户使用指南",
-         src:"UserGuide",
-         id:2
-       },{
-        iconimg:"/img/profileicon1.png",
-         text:"用户反馈窗口",
-         src:"UserFeedback",
-         id:3
-       },],
+       
+        
+        
+      },
+      
      }
    },
    created() {
@@ -50,63 +62,37 @@ export default {
 
    
    methods:{
-    routerGo(a){
-      this.isClick=false;
-      this.$router.push({ name:`${a.src}`, params: { userId: 123 }})
-      console.log(a);
-      
+     changeFatherB(){
+      this.$emit('resetFatherp');
     },
-      resetF(){
-        console.log("chufal!");
-        this.$router.push('/HelpAndfeedback');
-       this.isClick=!this.isClick;
-     }
+   resetF(){
+
+          this.$router.push('/mydownload');
+          this.isShow=!this.isShow;
+      }
    }
 }
 </script>
 <style lang="scss" scoped>
-.HelpAndfeedback{
-
-  &-header{
-    margin-top: 0.4rem;
-    height: 40px;
-    line-height: 40px;
-    text-align: center;
-    border-bottom: 1px solid #cccccc;
-    background: #ffffff;
+.Commonquestion-title{
+  margin-top: 0.4rem;
+  height: 40px;
+  line-height: 40px;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  &-back{
+  margin-left: 0.2rem;
   }
- 
-}
-.menuHelpAndfeedback{
+  &-text{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%)
+  }
   
-  li{
-    
-    list-style: none;
-    padding-left: 20px;
-    height: 50px;
-    line-height: 50px;
-    background: #ffffff;
-    &:nth-child(3n){
-      margin-bottom: 10px;
-      border-bottom: 1px solid #cccccc;
-    }
-    &:nth-child(odd){
-    border-top: 1px solid #cccccc;
-    }
-    &:nth-child(even){
-    border-top: 1px solid #cccccc;
-    }
-    &:last-child{
-      border-bottom: 1px solid #cccccc;
-    }
-    img{
-      width: 24px;
-      margin-left: 10px;
-      margin-right: 10px;
-      padding-top: 14px;
-      padding-bottom: 14px;
-    }
-  }
-
 }
+
+
 </style>
