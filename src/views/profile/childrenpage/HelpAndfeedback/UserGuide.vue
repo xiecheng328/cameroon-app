a17a04346fd902faf353b901176ce17e4c239366
wujvsen <40513464+wujvsen@users.noreply.github.com>
2018-10-24 10:51:31 +0800

    Update UserGuide.vue

--- src/views/profile/childrenpage/HelpAndfeedback/UserGuide.vue
+++ src/views/profile/childrenpage/HelpAndfeedback/UserGuide.vue
@@ -5,28 +5,28 @@
       <!-- 用户使用指南title -->
       <mu-appbar style="width: 100%;left:0;right:0;top:0;position:fixed;" color="#FFFFFF" z-depth="0" textColor="#333333">
       
-      <div class="help-title">{{userG}}</div>
+      <div class="help-title">{{iserGuide.userG}}</div>
       <mu-button  icon slot="right" ></mu-button>
       </mu-appbar>
       <p></p>
        
   </div>
   <div class="con">
-    <div   style="color:red;margin-top:1rem">{{cirC}}</div>
+    <div   style="color:red;margin-top:1rem">{{iserGuide.cirC}}</div>
              <div  style="color:red;margin-top:20px"></div>
-            <span> {{introd}}</span> 
+            <span> {{iserGuide.introd}}</span> 
 
-             <div   style="color:red;margin-top:1.5rem">{{infotM}}</div>
+             <div   style="color:red;margin-top:1.5rem">{{iserGuide.infotM}}</div>
              <div  style="color:red;margin-top:20px"></div>
-            <span> {{ infotFI }}</span> 
+            <span> {{ iserGuide.infotFI }}</span> 
 
-             <div   style="color:red;margin-top:1.5rem">{{mall}}</div>
+             <div   style="color:red;margin-top:1.5rem">{{iserGuide.mall}}</div>
              <div  style="color:red;margin-top:20px"></div>
-            <span> {{ mallF }}</span> 
+            <span> {{ iserGuide.mallF }}</span> 
 
-             <div   style="color:red;margin-top:1.5rem">{{mine}}</div>
+             <div   style="color:red;margin-top:1.5rem">{{iserGuide.mine}}</div>
              <div  style="color:red;margin-top:20px"></div>
-            <span>{{ myFu }}</span> 
+            <span>{{ iserGuide.myFu }}</span> 
            </div>
                 
           
@@ -40,41 +40,19 @@
 export default {
    data(){
       return{ 
-            pageMessage1:{
+            iserGuide:{
                 userG:this.$t('profile.userGuide'),
-            },
-             pageMessage2:{
                 cirC:this.$t('profile.cirC'),
-            },
-             pageMessage3:{
                 infotM:this.$t('profile.infotM'),
-            },
-             pageMessage4:{
                 mall:this.$t('profile.mall'),
-            },
-             pageMessage5:{
                 mine:this.$t('profile.mine'),
-            },
-             pageMessage6:{
                 introd:this.$t('profile.introd'),
-            },
-             pageMessage7:{
                 infotFI:this.$t('profile.infotFI'),
-            },
-             pageMessage8:{
                 mallF:this.$t('profile.mallF'),
-            },
-             pageMessage9:{
                 myFu:this.$t('profile.myFu'),
             },
-        
-     
-   
-
-   
-
-  name:"UserGuide"
-
+       
+  name:{"UserGuide"},
       }
   }
 }
