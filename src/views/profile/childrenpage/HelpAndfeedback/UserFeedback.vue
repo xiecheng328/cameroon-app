79758956c87ed0fad4e48a8676755c2487e9a62d
wujvsen <40513464+wujvsen@users.noreply.github.com>
2018-10-24 10:52:17 +0800

    Update UserFeedback.vue

--- src/views/profile/childrenpage/HelpAndfeedback/UserFeedback.vue
+++ src/views/profile/childrenpage/HelpAndfeedback/UserFeedback.vue
@@ -5,41 +5,41 @@
       <!-- 用户反馈窗口title -->
       <mu-appbar style="width: 100%;left:0;right:0;top:0;position:fixed;" color="#FFFFFF" z-depth="0" textColor="#333333">
       
-      <div class="help-title">{{userFeedback}}</div>
+      <div class="help-title">{{iserFeedback. userFeedback}}</div>
       <mu-button  icon slot="right" ></mu-button>
       </mu-appbar>
        
   </div>
   <div class="con">
-    <div   style="color:red;margin-top:1rem">{{pleaseSmyFu}}</div>
+    <div   style="color:red;margin-top:1rem">{{iserFeedback. pleaseSmyFu}}</div>
             <div>
                 <select v-model="selected">
                     <option disabled value=""></option>
-                    <option>{{pleaseC}}</option>
-                    <option>{{cirC}}</option>
-                    <option>{{infotM}}</option>
-                    <option>{{mall}}</option>
-                    <option>{{mine}}</option>
+                    <option>{{iserFeedback. pleaseC}}</option>
+                    <option>{{iserFeedback. cirC}}</option>
+                    <option>{{iserFeedback. infotM}}</option>
+                    <option>{{iserFeedback. mall}}</option>
+                    <option>{{iserFeedback. mine}}</option>
                 </select>
               
            </div>
-                       <div  style="color:red;margin-top:20px">{{feedCon}}</div>
-            <textarea v-model="message" placeholder="{{pleaseEditH}}~"></textarea>
+                       <div  style="color:red;margin-top:20px">{{iserFeedback. feedCon}}</div>
+            <textarea v-model="message" placeholder="{{iserFeedback. pleaseEditH}}~"></textarea>
             
 
-               <div  style="color:red;margin-top:20px">{{pleaseLP}}</div>
+               <div  style="color:red;margin-top:20px">{{iserFeedback. pleaseLP}}</div>
            <input v-model="msg" />
 
-              <div  style="color:red;margin-top:20px">{{pleaseLEM}}</div>
+              <div  style="color:red;margin-top:20px">{{iserFeedback. pleaseLEM}}</div>
            <input v-model="msg2" />
 <br>
            </div>
           
-           <button class="button-help">{{clickSub}}</button>
+           <button class="button-help">{{iserFeedback. clickSub}}</button>
           
 </div>
 
-            
+    
 </template>
 <script>
 export default {
@@ -49,50 +49,27 @@ export default {
          checked: true,
             checkedNames: ['John'],
             picked: 'One',
-            selected: '{{pleaseC}}',
+            selected: '{{iserFeedback.pleaseC}}',
             selectedMulit:['B','C'],
             selectedAutoSign: 'A',
-            msg:"{{pleaseEpPh}}",
-            msg2:"{{pleaseEMail}}",  
-            pageMessage1:{
+            msg:"{{iserFeedback.pleaseEpPh}}",
+            msg2:"{{iserFeedback.pleaseEMail}}",  
+            iserFeedback:{
                 userFeedW:this.$t('profile.userFeedback'),
+                 pleaseS:this.$t('profile.pleaseS'),
+                 pleaseC:this.$t('profile.pleaseC'),
+                 cirC:this.$t('profile.cirC'),
+                 infotM:this.$t('profile.infotM'),
+                 mall:this.$t('profile.mall'),
+                 mine:this.$t('profile.mine'),
+                 feedCon:this.$t('profile.feedCon'),
+                 pleaseLP:this.$t('profile.pleaseLP'),
+                 pleaseLEM:this.$t('profile.pleaseLEM'),
+                 clickSub:this.$t('profile.clickSub'),
+                 pleaseEpPh:this.$t('profile.pleaseEpPh'),
+                 pleaseEMail:this.$t('profile.pleaseEMail'),
             },
-             pageMessage2:{
-                pleaseS:this.$t('profile.pleaseS'),
-            },
-             pageMessage3:{
-                pleaseC:this.$t('profile.pleaseC'),
-            },
-             pageMessage4:{
-                cirC:this.$t('profile.cirC'),
-            },
-             pageMessage5:{
-                infotM:this.$t('profile.infotM'),
-            },
-             pageMessage6:{
-                mall:this.$t('profile.mall'),
-            },
-             pageMessage7:{
-                mine:this.$t('profile.mine'),
-            },
-             pageMessage8:{
-                feedCon:this.$t('profile.feedCon'),
-            },
-             pageMessage9:{
-                pleaseLP:this.$t('profile.pleaseLP'),
-            },
-             pageMessage10:{
-                pleaseLEM:this.$t('profile.pleaseLEM'),
-            },
-             pageMessage11:{
-                clickSub:this.$t('profile.clickSub'),
-            },
-             pageMessage12:{
-                pleaseEpPh:this.$t('profile.pleaseEpPh'),
-            },
-             pageMessage13:{
-                pleaseEMail:this.$t('profile.pleaseEMail'),
-            },
+          
         imgList:[
         
         ],
