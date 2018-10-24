b97068b8b0ce43f813b0564ee599832438f0f2a6
Lsunshineandrainbow <3480782290@qq.com>
2018-10-24 11:40:08 +0800

    language

--- src/views/message/Withme.vue
+++ src/views/message/Withme.vue
@@ -2,26 +2,25 @@
 <div id="withme">
  <div id='messageTitle'>
   <span style="margin-left: .1rem" @click="back" ><-</span>
-  <h5>@Je</h5>
+  <h5>@{{pagemessage.withme}}</h5>
  </div>
  <div class="titlehead">
   <img class="messagehead" src="../../assets/img/messagesHead.png" alt="">
    <ul>
     <li>
-     Spécialiste de riz
+     {{pagemessage.withmerice}}
     </li>
     <li>
      5-22 16:40
     </li>
    </ul>
  </div>
- <p>//@Spécialiste de riz ：Le message de transfert</p>
+ <p>//@{{pagemessage.withmerice}} ：{{pagemessage.withmetransfert}}</p>
   <div class="messagecontain">
    <img class="messageconent" src="../../assets/img/messageConent.png" alt="">
    <div class="containtext">
-   <span>@Expert de maïs</span>
-   <div class="containtext">Le maïs à 40 cm, pour le maïs est appliquée, de fumier, de la promotion de la croissance
-
+   <span>@{{pagemessage.withmepersonB}}</span>
+   <div class="containtext">{{pagemessage.withmecorn}}
    </div>
    </div>
  </div>
@@ -29,27 +28,38 @@
   <img class="messagehead" src="../../assets/img/messagesHead.png" alt="">
   <ul>
    <li>
-    Expert de soja
+    {{pagemessage.withmepersonA}}
    </li>
    <li>
     5-22 16:40
    </li>
   </ul>
  </div>
- <p>Commentaires@Spécialiste de riz</p>
+ <p>{{pagemessage.withmecomment}}@{{pagemessage.withmepersonA}}</p>
  <div class="messagecontain">
   <img class="messageconent" src="../../assets/img/messageConent.png" alt="">
   <div class="containtext">
-   <span>@Expert de soja</span>
+   <span>@{{pagemessage.withmepersonC}}</span>
    <div class="containtext">
-    La plantation de riz doit d'abord, puis à traiter le lit de semences de riz paddy, après traitement de stérilisation et à la désinfection de graine de nouveau, après transplantation de semis de riz, on a à faire, c'est de combler les lacunes et de consultation de l'irrigation, de suivi et d'engrais, de travail et de LIRIOMYZA de feuilles de prévention.
-   </div>
+    {{pagemessage.withmerice}}
   </div>
  </div>
 </div>
 </template>
 <script>
  export default {
+     data(){
+         return{
+             withmepersonA:this.$t("message.withmepersonA"),
+             withmepersonB:this.$t("message.withmepersonB"),
+             withmepersonC:this.$t("message.withmepersonC"),
+             withmecorn:this.$t("message.withmecorn"),
+             withmerice:this.$t("message.withmerice"),
+             withmecomment:this.$t("message.withmecomment"),
+             withme:this.$t("message.withme"),
+             withmetransfert:this.$t("message.withmetransfert"),
+         }
+     },
      methods:{
          back(){
               this.$router.push('/message');
--- src/views/message/newdetail.vue
+++ src/views/message/newdetail.vue
@@ -1,19 +1,12 @@
 <template>
     <div>
         <div id="detailhead">
-            <h2 id="detailreturn" @click="detailback()"><--</h2>    <h2> numéro d'abonnement</h2>
+            <h2 id="detailreturn" @click="detailback()"><--</h2>    <h2> {{{pagemessage.newdetailSubscribe}}</h2>
         </div>
         <div id="detailtext">
-            <h6 style="margin: 0 auto ;width: 2rem">Utilisation de machines agricoles</h6>
-            <div> Avec la généralisation de la mécanisation agricole, de machines agricoles apparaissent de plus en plus les maisons, la question de l'utilisation de nouvelles machines agricoles est très important.<br/>
-
-                　　C'est de lire la notice.Dans le nouveau acheter de nouvelles machines, le moteur avant de l'utiliser, il faut d'abord lire notice sérieusement à l'aide de ladite machine, une attention particulière doit être d'utiliser les dispositions de marques de diesel, avant d'utiliser pleinement la précipitation et de filtrage, un dispositif de ravitaillement en carburant à nettoyer.De l'huile de lubrification à nettoyer, et à ajouter de l'huile doit être suffisamment et régulièrement de remplacement, l'utilisation de la marque doit être conforme à la réglementation.Filtre à air pour toujours attention à l'entretien, à l'attention de ne pas ajouter de l'eau de refroidissement, et, en particulier, de sorte que le moteur est à l'état de fonctionnement de la pénurie d'eau.Toujours vérifier, la fixation des boulons, écrous, en particulier le boulon de bielle, un arbre d'entraînement du volant sur le boulon et l'écrou de blocage, un écrou de fixation de roue.<br/>
-
-                Le deuxième est l'essai de rodage.L'achat de nouvelles machines, pas tout de suite avec la charge de travail, doit être en conformité avec les instructions de fonctionnement d'essai de rodage.Après la fin de la machine de meulage, de nettoyage de filtre d'huile et le retrait de l'huile dans les impuretés résiduelles, il est nécessaire également pour son huile de remplacement et de nettoyage de carter d'huile.<br/>
-                　　Ce n'est pas à la charge de travail.Lorsque la nouvelle machine de travail, doit être strictement conformément aux exigences des instructions du fabricant, de ne pas surcharger, pas d'excès de vitesse, pas de super - alimentation, pas à long terme de la charge de travail.<br/>
-
-                Quatre, c'est pas libre pour de nouvelles machines de réparer et arbitraire de réglage à vitesse de rotation élevée du moteur.Certains utilisateurs afin de permettre à la machine de courir plus vite, de la quantité d'alimentation en huile de pression d'alimentation en carburant et éventuellement de régulation de haute pression de la pompe à huile, certains également un ressort de régulation de vitesse avec du fil de ligature, c'est une erreur.<br/>
-                　　L'utilisation rationnelle de nouvelles machines agricoles, la réduction de défaillance mécanique, de prolonger la durée de vie de machines, d'améliorer la production agricole.<br/>
+            <h6 style="margin: 0 auto ;width: 2rem">{{pagemessage.newdetailtitle}}</h6>
+            <div>
+                {{pagemessage.newdetailtext}}
             </div>
 
         </div>
@@ -21,6 +14,14 @@
 </template>
 <script>
     export default {
+        data(){
+            return{
+                newdetailSubscribe:this.$t("message.newdetailSubscribe"),
+                newdetailtitle:this.$t("message.newdetailtitle"),
+                newdetailtext:this.$t("message.newdetailtext"),
+
+            }
+        },
         methods:{
             detailback(){
                 this.$router.push('openSubScription_content');
